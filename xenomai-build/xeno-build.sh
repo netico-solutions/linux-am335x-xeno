21fbf22308b3ed99b2b16489d33aae08fcb05294
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-08-15 19:39:32 +0200

    xeno-build.sg script was missing make command

--- xenomai-build/xeno-build.sh
+++ xenomai-build/xeno-build.sh
@@ -3,5 +3,7 @@
 cflags="-march=armv7-a -mfpu=vfp3"
 ldflags="-march=armv7-a -mfpu=vfp3"
 host_compiler="arm-linux-gnueabihf"
+staging_path="/srv/nfs/testing"
 
 ../xenomai-2.6.2.1/configure CFLAGS=$cflags LDFLAGS=$ldflags --host=$host_compiler
+make DESTDIR=$staging_path
